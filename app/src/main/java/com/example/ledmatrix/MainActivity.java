5080ab72750dee3b1fe082e92607e95e5c4377b0
Jonathan Avila <jeavila6@miners.utep.edu>
2019-03-28 11:37:55 -0600

    added toolbar; added themes for tabs and toolbar

--- app/src/main/java/com/example/ledmatrix/MainActivity.java
+++ app/src/main/java/com/example/ledmatrix/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.ledmatrix;
 
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set toolbar as app bar
+        Toolbar mainToolbar = findViewById(R.id.main_toolbar);
+        setSupportActionBar(mainToolbar);
 
         // supply ViewPager with PagerAdapter implementation
         FragmentManager fragmentManager = getSupportFragmentManager();
