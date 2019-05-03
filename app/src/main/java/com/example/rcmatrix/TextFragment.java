0f28bc78ea3a41948c0666ea2b5f6dab74bb2d18
Jonathan Avila <jeavila6@miners.utep.edu>
2019-05-02 20:06:24 -0600

    show color picker when tapping color

--- app/src/main/java/com/example/rcmatrix/TextFragment.java
+++ app/src/main/java/com/example/rcmatrix/TextFragment.java
@@ -4,28 +4,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-public class TextFragment extends Fragment {
+import petrov.kristiyan.colorpicker.ColorPicker;
 
-    EditText mInputEditText;
-    EditText mColorEditText;
+public class TextFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_text, container, false);
-        mInputEditText = view.findViewById(R.id.input_edit_text);
-        mColorEditText = view.findViewById(R.id.color_edit_text);
+
+        ImageButton colorImageButton = view.findViewById(R.id.color_image_button);
+        colorImageButton.setOnClickListener(v -> showColorPicker());
+
         return view;
     }
 
-    public byte[] getMessage() {
-        String message = mColorEditText.getText().toString(); // text color
-        message += mInputEditText.getText().toString(); // text
-        return message.getBytes();
+    private void showColorPicker() {
+
+        ColorPicker colorPicker = new ColorPicker(getActivity());
+        colorPicker.show();
+
+        colorPicker.setOnChooseColorListener(new ColorPicker.OnChooseColorListener() {
+            @Override
+            public void onChooseColor(int position, int color) {
+                // ...
+            }
+
+            @Override
+            public void onCancel(){
+                // ...
+            }
+        });
     }
 
+    public byte[] getMessage() {
+        String message = "empty";
+        return message.getBytes();
+    }
 }
