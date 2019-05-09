fb389a3a8f51bf6441a97a8700983df1df6dff0d
Jonathan Avila <jeavila6@miners.utep.edu>
2019-05-08 23:13:13 -0600

    updated about information; links in about dialog are clickable

--- app/src/main/java/com/example/rcmatrix/AboutDialogFragment.java
+++ app/src/main/java/com/example/rcmatrix/AboutDialogFragment.java
@@ -4,8 +4,10 @@ import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
@@ -16,18 +18,20 @@ class AboutDialogFragment extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        // inflate layout for dialog, passing null as parent view since its used in dialog layout
+        // inflate layout
         LayoutInflater inflater = requireActivity().getLayoutInflater();
         @SuppressLint("InflateParams") View dialogView =
                 inflater.inflate(R.layout.fragment_dialog_about, null);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity(), R.style.DialogTheme);
-
-        // set constructed layout for dialog
         builder.setView(dialogView);
 
-        // set positive button for dialog
+        // set positive button; do nothing on click
         builder.setPositiveButton(R.string.about_dialog_positive_button, (dialog, id) -> {});
+
+        // make links clickable
+        TextView aboutTextView = dialogView.findViewById(R.id.about_text_view);
+        aboutTextView.setMovementMethod(LinkMovementMethod.getInstance());
 
         return builder.create();
     }
